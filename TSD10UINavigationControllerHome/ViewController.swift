773828e7502fee2a8644fccfbc158b140a53bdce
GerashchenkoDmitry <finiqw@gmail.com>
2022-07-11 10:42:30 +0700

    Initial Commit

---
+++ TSD10UINavigationControllerHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TSD10UINavigationControllerHome
+//
+//  Created by Дмитрий Геращенко on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
